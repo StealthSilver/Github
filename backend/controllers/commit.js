50cb8e21e0aa545ec8faf6f9d023e7d79c3216ae
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-15 12:57:29 +0530

    init

--- backend/controllers/commit.js
+++ backend/controllers/commit.js
@@ -3,6 +3,24 @@ const path = require("path");
 // to give new id to every commit
 const { v4: uuidv4 } = require("uuid");
 
-async function commitRepo(message) {}
+async function commitRepo(message) {
+  const repoPath = path.resolve(process.cwd(), ".hiddenGit");
+  const stagedPath = path.join(repoPath, "staging");
+  const commitPath = path.join(repoPath, "commits");
+}
+
+try {
+  const commitID = uuidv4();
+  const commitDir = path.join(commitPath, commitID);
+  await fs.mkdir(commitDir, { recursive: true });
+
+  const files = await fs.readdir(stagedPath);
+
+  for (const file of files) {
+    await fs.copyFile(path.join(stagedPath, file), path.join(commitDir, file));
+  }
+} catch (err) {
+  console.error("Error commititng files: ", err);
+}
 
 module.exports = { commitRepo };
