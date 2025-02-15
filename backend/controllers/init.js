ca10a150b3ee24036e739622279c80877ee79d74
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-15 12:23:35 +0530

    init

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,2 +1,12 @@
 const fs = require("fs").promises;
 const path = require("path");
+
+async function initRepo() {
+  const repoPath = path.resolve(process.cwd(), ".hiddenGit");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+  } catch (err) {}
+}
+
+module.exports = { initRepo };
