9d22f07abd2d5f633a702912b2188092751b78b5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-15 12:38:21 +0530

    init

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -9,6 +9,7 @@ async function addRepo(filePath) {
     await fs.mkdir(stagingPath, { recursive: true });
     const fileName = path.basename(filePath);
     await fs.copyFile(filePath, path.join(stagingPath, fileName));
+    console.log(`File ${fileName} added to the staging area`);
   } catch (err) {
     console.error("Error adding file : ", err);
   }
